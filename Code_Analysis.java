acddc02290e1251ec0cf11d5b9684544904b63c3
Lelethu <u13028023@tuks.co.za>
2015-07-09 10:01:09 +0200

    FlowchartManager

--- Code_Analysis.java
+++ Code_Analysis.java
@@ -11,3 +11,12 @@
 //but from current evaluation it appears to be the actual pane onto which flowcharts are drawn
 - These tabs to access the sheets are seen in the JavaBlock GUI at the bottom (line 31), normally named the same as the flowchart Start node name
 - The Workspace contains the multiple Sheets in a List Structure, this structure of course allows for the removal and addition of the Sheets
+
+*FlowchartManager.java
+- This class appears to be responsible for the following:
+	- What happens when the user presses the close button to close the application  (close dialog)
+	- What happens when the user opens the application (loading the last opened project, parsing its entire state from XML)
+	- What happens while the user is busy with the application, if a user clicks on a block, it focuses the blocks and opens the responsible editors in the left pane (line 791)
+	- Keeps track of the user history such that redo and undo actions can carry through their actions by reading from the history and restoring the state of the Sheet 
+	- Loading the workspace when the user opens the application
+
