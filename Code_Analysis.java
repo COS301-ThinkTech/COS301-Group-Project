50c19ef54bf0e2957075f93edb817445b51ae5ee
Lelethu <u13028023@tuks.co.za>
2015-07-09 10:13:35 +0200

    Sheet

--- Code_Analysis.java
+++ Code_Analysis.java
@@ -15,8 +15,12 @@
 *FlowchartManager.java
 - This class appears to be responsible for the following:
 	- What happens when the user presses the close button to close the application  (close dialog)
-	- What happens when the user opens the application (loading the last opened project, parsing its entire state from XML)
+	- What happens when the user opens the flowchart (loading the last opened flowchart, parsing its entire state from XML)
 	- What happens while the user is busy with the application, if a user clicks on a block, it focuses the blocks and opens the responsible editors in the left pane (line 791)
 	- Keeps track of the user history such that redo and undo actions can carry through their actions by reading from the history and restoring the state of the Sheet 
 	- Loading the workspace when the user opens the application
+	
+*Sheet.java
+- Each Sheet has a FlowchartManager
+- The Sheet is the unit on which the user creates the flowchart
 
