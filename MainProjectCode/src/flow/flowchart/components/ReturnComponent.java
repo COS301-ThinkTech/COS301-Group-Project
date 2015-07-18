6ba20aa4f057a748c4cfc4070ef2f70577b763f0
tshepiso <tshepimagagula@gmail.com>
2015-07-18 02:12:14 +0200

    ReturnComponent icon update.

---
+++ MainProjectCode/src/flow/flowchart/components/ReturnComponent.java
@@ -0,0 +1,33 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package flow.flowchart.components;
+
+import java.awt.Component;
+import java.awt.Graphics;
+import javax.swing.Icon;
+
+/**
+ *
+ * @author tshepiso
+ */
+public class ReturnComponent implements Icon{
+
+    @Override
+    public void paintIcon(Component cmpnt, Graphics g, int i, int i1) {
+       
+    }
+
+    @Override
+    public int getIconWidth() {
+       return 20;
+    }
+
+    @Override
+    public int getIconHeight() {
+       return 20; 
+    }
+    
+}
