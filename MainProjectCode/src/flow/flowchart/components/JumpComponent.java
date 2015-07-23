697d9d0ca6cbefea02dd46dd3afa9b3cc4ba17bc
Lelethu <u13028023@tuks.co.za>
2015-07-23 11:36:31 +0200

    Editing

--- MainProjectCode/src/flow/flowchart/components/JumpComponent.java
+++ MainProjectCode/src/flow/flowchart/components/JumpComponent.java
@@ -1,12 +1,11 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
+
 package flow.flowchart.components;
 
-import java.awt.*;
-import javax.swing.*;
+import java.awt.Color;
+import java.awt.Component;
+import java.awt.Graphics;
+import java.awt.Graphics2D;
+import javax.swing.Icon;
 
 /**
  *
