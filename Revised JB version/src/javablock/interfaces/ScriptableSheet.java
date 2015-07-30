a97721fbe5e6a845ef077b838e31a3fe1fbf732d
Lelethu <u13028023@tuks.co.za>
2015-07-30 21:30:48 +0200

    Interface

---
+++ Revised JB version/src/javablock/interfaces/ScriptableSheet.java
@@ -0,0 +1,14 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package javablock.interfaces;
+
+/**
+ *
+ * 
+ */
+public interface ScriptableSheet {
+    public String getScript(String lang);
+    public Boolean isStepByStep();
+}
