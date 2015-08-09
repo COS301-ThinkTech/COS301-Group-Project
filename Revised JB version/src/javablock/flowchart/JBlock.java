c745a043c7d516ac6a706ce1e53bbdf421d729a4
Lelethu <u13028023@tuks.co.za>
2015-08-09 10:36:57 +0200

    Creation of ForLoopBlock and Cases

--- Revised JB version/src/javablock/flowchart/JBlock.java
+++ Revised JB version/src/javablock/flowchart/JBlock.java
@@ -29,7 +29,7 @@ public abstract class JBlock implements FlowElement{
         IO, IOin, IOout, CPU, SCRIPT,
         DECISION, COMMENT, LINK,
         BRACE, JUMP, NULL, GROUP, 
-        LOGO, CANVAS2D, STRUCT, CUSTOM, MODULE
+        LOGO, CANVAS2D, STRUCT, CUSTOM, MODULE, FORLOOP
     };
     static final ArrayList<Class<JBlock>> customTypes=new ArrayList<Class<JBlock>>();
     static HashMap<Class, String> customTypesNames=new HashMap<Class, String>();
@@ -55,7 +55,7 @@ public abstract class JBlock implements FlowElement{
         return Type.CUSTOM;
     }
     public static Type StandardTypes[]={
-        Type.CPU/*, Type.IOin, Type.IOout*/, Type.DECISION, Type.RETURN, Type.MODULE/*, Type.COMMENT*/
+        Type.CPU/*, Type.IOin, Type.IOout*/, Type.DECISION, Type.RETURN, Type.MODULE, Type.FORLOOP/*, Type.COMMENT*/
     };
     public static Type HelpingTypes[]={
         Type.IO, Type.JUMP, Type.LINK, Type.BRACE/*, Type.STRUCT*/, Type.SCRIPT
@@ -65,7 +65,7 @@ public abstract class JBlock implements FlowElement{
     };
     
     public static String leftLineTypes=
-        "CPU IOin IOout DECISION RETURN COMMENT, MODULE";
+        "CPU IOin IOout DECISION RETURN COMMENT MODULE FORLOOP";
     public static String rightLineTypes=
         "IO JUMP LINK BRACE STRUCT SCRIPT";
     public static String[] getLeftLine(){
@@ -157,6 +157,7 @@ public abstract class JBlock implements FlowElement{
             case SCRIPT: b= new scrBlock(parent); break;
             case COMMENT: b= new commentBlock(parent); break;
             case MODULE: b= new moduleBlock(parent);  break;
+            case FORLOOP: b= new forloopBlock(parent);  break;
                 
             case LINK: b= new linkBlock(parent); break;
             case BRACE: b= new braceBlock(parent); break;
