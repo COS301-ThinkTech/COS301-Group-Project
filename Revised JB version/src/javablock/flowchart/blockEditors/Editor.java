649d97270be5f7cde2dae04c904241adfed3a210
Lelethu <u13028023@tuks.co.za>
2015-08-11 02:19:27 +0200

    Editor Refactor

---
+++ Revised JB version/src/javablock/flowchart/blockEditors/Editor.java
@@ -0,0 +1,8 @@
+package javablock.flowchart.blockEditors;
+
+import javablock.Sheet;
+
+public class Editor {
+    public static Sheet flow;
+    public static standardEditor standard=new standardEditor();
+}
