45e7c0adad816489abca5c14439b6224b3ff58d1
hlavutelo <hlavutelo.maluleke@gmail.com>
2015-08-13 22:17:16 +0200

    Comment block tester

---
+++ Unit Testing/Revised JB version/test/javablock/flowchart/blocks/CommentBlockTest.java
@@ -0,0 +1,31 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package javablock.flowchart.blocks;
+
+import junit.framework.TestCase;
+
+/**
+ *
+ * @author Hlavutello
+ */
+public class CommentBlockTest extends TestCase {
+    
+    public CommentBlockTest(String testName) {
+        super(testName);
+    }
+
+    /**
+     * Test of shape method, of class CommentBlock.
+     */
+    public void testShape() {
+        System.out.println("shape");
+        CommentBlock instance = null;
+        instance.shape();
+        // TODO review the generated test code and remove the default call to fail.
+        fail("The test case is a prototype.");
+    }
+    
+}
