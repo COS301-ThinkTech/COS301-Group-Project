bf48d85c544712901ab51a5d93b71b9f8b8beff5
Lelethu <u13028023@tuks.co.za>
2015-10-03 20:22:23 +0200

    [Blocks ToolBar]: Added separators between the block buttons

--- Revised JB version/src/javablock/flowchart/BlocksToolBar.java
+++ Revised JB version/src/javablock/flowchart/BlocksToolBar.java
@@ -5,8 +5,12 @@
 package javablock.flowchart;
 
 import config.translator;
+import java.awt.Color;
 import java.awt.Dimension;
+import java.awt.FlowLayout;
+import java.awt.GridLayout;
 import javax.swing.JButton;
+import javax.swing.JPopupMenu;
 import javax.swing.JToolBar;
 
 
@@ -20,6 +24,8 @@ public class BlocksToolBar extends JToolBar{
         JToolBar blocks = new JToolBar("Blocks");
         blocks.setFloatable(false);
         blocks.setOrientation(JToolBar.HORIZONTAL);
+        
+        
         {
             JToolBar std = new JToolBar("Standard");
             std.setFloatable(false);
@@ -51,7 +57,8 @@ public class BlocksToolBar extends JToolBar{
                 }
                 b.setPreferredSize(new Dimension(40, 40));
                 b.setIcon(new javax.swing.ImageIcon(JBlock.getIcon(T)));
-                std.add(b);
+                std.add(b);                
+                std.add(new JPopupMenu.Separator());
                 b.addActionListener(flow);
                 b.addMouseMotionListener(flow);
             }
@@ -60,13 +67,15 @@ public class BlocksToolBar extends JToolBar{
                 b.setToolTipText(translator.tooltips.getString(T.toString() + ".help"));
                 b.setActionCommand("add/" + T.toString());
                 b.setPreferredSize(new Dimension(32, 32));
-                b.setIcon(new javax.swing.ImageIcon(JBlock.getIcon(T)));
+                b.setIcon(new javax.swing.ImageIcon(JBlock.getIcon(T)));                
                 std.add(b);
+                std.add(new JPopupMenu.Separator());
                 b.addActionListener(flow);
                 b.addMouseMotionListener(flow);
             }
             blocks.add(std);
-            add(blocks);       
+            add(blocks);
+            addSeparator();
             validate();
             revalidate();        
         }
