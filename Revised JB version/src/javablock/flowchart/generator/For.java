01d78a8aaab02d5ed7301ce361b2695e80dcd514
Lelethu <u13028023@tuks.co.za>
2015-10-17 08:44:23 +0200

    Removal of "declare variable" Option in Loop Generator

--- Revised JB version/src/javablock/flowchart/generator/For.java
+++ Revised JB version/src/javablock/flowchart/generator/For.java
@@ -34,8 +34,6 @@ public class For extends javax.swing.JPanel implements Generator{
         comp.setSelectedIndex(0);
         iterType.setSelectedIndex(0);
         iterNum.setText("1");
-
-        declare.setSelected(true);
     }
     @Override
     public JBlock[] get(Flowchart f){
@@ -118,7 +116,6 @@ public class For extends javax.swing.JPanel implements Generator{
         comp = new javax.swing.JComboBox();
         iterNum = new javax.swing.JTextField();
         iterType = new javax.swing.JComboBox();
-        declare = new javax.swing.JCheckBox();
         jLabel3 = new javax.swing.JLabel();
 
         java.util.ResourceBundle bundle = java.util.ResourceBundle.getBundle("config/lang/lang"); // NOI18N
@@ -142,14 +139,6 @@ public class For extends javax.swing.JPanel implements Generator{
 
         iterType.setModel(new javax.swing.DefaultComboBoxModel(new String[] { "+", "-", "*", "/" }));
 
-        declare.setSelected(true);
-        declare.setText(bundle.getString("generator.for.declare")); // NOI18N
-        declare.addActionListener(new java.awt.event.ActionListener() {
-            public void actionPerformed(java.awt.event.ActionEvent evt) {
-                declareActionPerformed(evt);
-            }
-        });
-
         jLabel3.setText("=");
 
         javax.swing.GroupLayout layout = new javax.swing.GroupLayout(this);
@@ -160,12 +149,9 @@ public class For extends javax.swing.JPanel implements Generator{
                 .addContainerGap()
                 .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
                     .addGroup(javax.swing.GroupLayout.Alignment.TRAILING, layout.createSequentialGroup()
-                        .addComponent(declare)
-                        .addGap(79, 79, 79))
-                    .addGroup(javax.swing.GroupLayout.Alignment.TRAILING, layout.createSequentialGroup()
-                        .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.TRAILING)
-                            .addComponent(jLabel5)
-                            .addComponent(jLabel4))
+                        .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
+                            .addComponent(jLabel4)
+                            .addComponent(jLabel5))
                         .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED)
                         .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING, false)
                             .addComponent(iterType, javax.swing.GroupLayout.PREFERRED_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.PREFERRED_SIZE)
@@ -207,19 +193,12 @@ public class For extends javax.swing.JPanel implements Generator{
                     .addComponent(jLabel5)
                     .addComponent(iterType, javax.swing.GroupLayout.PREFERRED_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.PREFERRED_SIZE)
                     .addComponent(iterNum, javax.swing.GroupLayout.PREFERRED_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.PREFERRED_SIZE))
-                .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED)
-                .addComponent(declare)
                 .addContainerGap(javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE))
         );
     }// </editor-fold>//GEN-END:initComponents
 
-    private void declareActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_declareActionPerformed
-        // TODO add your handling code here:
-    }//GEN-LAST:event_declareActionPerformed
-
     // Variables declaration - do not modify//GEN-BEGIN:variables
     private javax.swing.JComboBox comp;
-    private javax.swing.JCheckBox declare;
     private javax.swing.JTextField end;
     private javax.swing.JTextField initial;
     private javax.swing.JTextField iterNum;
